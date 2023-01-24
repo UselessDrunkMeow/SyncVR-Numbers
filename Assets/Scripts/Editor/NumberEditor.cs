373688bb3f5c8b8de102dc5e5fe42ff088e7c22c
UselessDrunkMeow <timvandergeest030204@gmail.com>
2023-01-24 15:51:24 +0100

    Number Conectors work
    The player can now select a number and connect it with the next number.

--- Assets/Scripts/Editor/NumberEditor.cs
+++ Assets/Scripts/Editor/NumberEditor.cs
@@ -6,47 +6,51 @@ using UnityEditor;
 [CustomEditor(typeof(PlacePoint))]
 public class NumberEditor : Editor
 {
-    [SerializeField] GameObject _NumberObject;
+    [SerializeField] GameObject _NumberObject;//the number object that needs to be instantiated
 
-    private NumberConector _Number;
-    private ListOfNumberPrefabs _List;
+    private NumberConector _Number;//A refrence to the instantiated object
+    private ListOfNumberPrefabs _List;//A list of all the numbers in the scene
+    private NumberEditor _NumberEditor;//A list of all the numbers in the scene
 
     private int _WhatNumber;
-    private GameObject _InstantiatedNumber;
 
     Vector3 _Spawnlocation;
     Quaternion _SpawnRotation;
 
-    //public void OnEnable()
-    //{
-    //    if (_Number == null)
-    //    {
-    //        _Number = target as NumberConector;
-    //    }
-    //}
     public override void OnInspectorGUI()
     {
         if (GUILayout.Button("SpawnNumber"))
         {
-            _Spawnlocation = FindObjectOfType<PlacePoint>().transform.position;
-            _List = FindObjectOfType<ListOfNumberPrefabs>();
+            _Spawnlocation = FindObjectOfType<PlacePoint>().transform.position;//Finds the placepoint object and sets the spawnlocation to tis location
+            _List = FindObjectOfType<ListOfNumberPrefabs>();//grabs a refrence to the list
 
-            _Number = Instantiate(_NumberObject, _Spawnlocation, _SpawnRotation).GetComponent<NumberConector>();
+            _Number = Instantiate(_NumberObject, _Spawnlocation, _SpawnRotation).GetComponent<NumberConector>();//instantiate the object and save it for later use
+
             _WhatNumber = _WhatNumber + 1;
             _Number._WhatNumber = _WhatNumber;
 
-            _List._NumberConectors.Add(_Number);            
+            _List._NumberConectors.Add(_Number);//Adds the instantiated object to the list
         }
 
         if (GUILayout.Button("RemoveNumber"))
-        {
-            _WhatNumber = _WhatNumber - 1;
-            _Number._WhatNumber = _WhatNumber;
-            DestroyImmediate(_List._NumberConectors[_Number._WhatNumber].gameObject);
-            _List._NumberConectors.RemoveAt(_Number._WhatNumber);       
-        }      
-        EditorUtility.SetDirty(_Number);
-        EditorUtility.SetDirty(_List);
+        {           
+            if (_WhatNumber <= 0)
+            {
+                _WhatNumber = 1;
+            }
+
+            if (_Number != null)
+            {
+                _WhatNumber = _WhatNumber - 1;
+                _Number._WhatNumber = _WhatNumber;
+                DestroyImmediate(_List._NumberConectors[_Number._WhatNumber].gameObject);//Destorys the object
+                _List._NumberConectors.RemoveAt(_Number._WhatNumber);//Removes the objcet from the list
+            }
+        }
+
+        EditorUtility.SetDirty(_Number);//saves the changes made to the object
+        EditorUtility.SetDirty(_List);//saves the changes made to the object
+        EditorUtility.SetDirty(_NumberEditor);//saves the changes made to the object
         base.OnInspectorGUI();
     }
 }
--- Assets/Scripts/NumberConector.cs
+++ Assets/Scripts/NumberConector.cs
@@ -7,12 +7,94 @@ public class NumberConector : MonoBehaviour
 {
     public int _WhatNumber;
     public TextMeshProUGUI _Number;
+
+    [SerializeField] LineRenderer _Line;
+
+    [SerializeField] bool _CanBeSelected;
+    int _AmountOfConnectedLines;
+
+    [SerializeField] NumberConector _Conector;
+
+    Vector2 _MousePos;
+    float _Distance;
+
+    [SerializeField] LayerMask layerMask;
+
+    [SerializeField] bool _DrawLine;
+
+    [SerializeField] Vector3 _CenterPoint;
     void Start()
     {
+        _CenterPoint = transform.position;
         _Number.text = _WhatNumber.ToString();
+        if (_WhatNumber == 1)
+        {
+            _CanBeSelected = true;
+        }
     }
- 
+
     void Update()
+    {
+        MousePos();
+        if (_CanBeSelected && _DrawLine)
+        {
+            DrawLine();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (_CanBeSelected)
+        {
+            _DrawLine = true;
+        }
+        //gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+    }
+    private void OnMouseUp()
+    {
+        if (_CanBeSelected)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(_MousePos, Vector3.forward, 5, layerMask);            
+            if (hit.collider)
+            {
+                _Conector = hit.transform.GetComponent<NumberConector>();
+            }
+
+            if( _Conector == null)
+            {
+                print("not correct :(");
+                _DrawLine = false;
+                _Line.enabled = false;
+            }
+
+            if (_Conector._WhatNumber == _WhatNumber + 1)
+            {
+                print("Correct!");
+                _DrawLine = false;
+                _CanBeSelected = false;
+                _Conector._CanBeSelected = true;
+            }
+            else
+            {
+                _Conector = null;
+                print("not correct :(");
+                _DrawLine = false;
+                _Line.enabled = false;
+            }
+
+        }      
+    }
+    void DrawLine()
+    {
+        _Line.enabled = true;
+        _Line.SetPosition(0, _CenterPoint);
+        _Line.SetPosition(1, _MousePos);
+    }
+    void MousePos()
+    {
+        _MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    void CheckCorrectLocation()
     {
 
     }
