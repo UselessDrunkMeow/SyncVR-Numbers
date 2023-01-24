0ec6e1d71ea72cef951562889cb945e1536b3222
UselessDrunkMeow <timvandergeest030204@gmail.com>
2023-01-24 17:35:27 +0100

    Easy level added. Started working on hard level

--- Assets/Scripts/Editor/NumberEditor.cs
+++ Assets/Scripts/Editor/NumberEditor.cs
@@ -7,6 +7,7 @@ using UnityEditor;
 public class NumberEditor : Editor
 {
     [SerializeField] GameObject _NumberObject;//the number object that needs to be instantiated
+    [SerializeField] GameObject _SmallNumberObject;//the number object that needs to be instantiated
 
     private NumberConector _Number;//A refrence to the instantiated object
     private ListOfNumberPrefabs _List;//A list of all the numbers in the scene
@@ -25,6 +26,18 @@ public class NumberEditor : Editor
             _List = FindObjectOfType<ListOfNumberPrefabs>();//grabs a refrence to the list
 
             _Number = Instantiate(_NumberObject, _Spawnlocation, _SpawnRotation).GetComponent<NumberConector>();//instantiate the object and save it for later use
+
+            _WhatNumber = _WhatNumber + 1;
+            _Number._WhatNumber = _WhatNumber;
+
+            _List._NumberConectors.Add(_Number);//Adds the instantiated object to the list
+        }
+        if (GUILayout.Button("SpawnSmallNumber"))
+        {
+            _Spawnlocation = FindObjectOfType<PlacePoint>().transform.position;//Finds the placepoint object and sets the spawnlocation to tis location
+            _List = FindObjectOfType<ListOfNumberPrefabs>();//grabs a refrence to the list
+
+            _Number = Instantiate(_SmallNumberObject, _Spawnlocation, _SpawnRotation).GetComponent<NumberConector>();//instantiate the object and save it for later use
 
             _WhatNumber = _WhatNumber + 1;
             _Number._WhatNumber = _WhatNumber;
